cf2444c3e8effaacce5c4cf101c7aa2f17553f4b
Maksim Makarov <mamakarov@avito.ru>
2024-08-29 01:54:20 +0300

    added tests

--- internal/app/delivery/http_test.go
+++ internal/app/delivery/http_test.go
@@ -3,10 +3,13 @@ package delivery
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"github.com/MisterMaks/go-yandex-gophermart/internal/app"
 	"github.com/MisterMaks/go-yandex-gophermart/internal/app/mock"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -424,6 +427,120 @@ func TestAppHandler_CreateOrder(t *testing.T) {
 			res := w.Result()
 
 			assert.Equal(t, tt.want.statusCode, res.StatusCode, "Invalid status code")
+		})
+	}
+}
+
+func TestAppHandler_GetOrders(t *testing.T) {
+	userIDWithOrders := uint(1)
+	userIDWithoutOrders := uint(2)
+
+	orderNumber := `12345`
+	status := "NEW"
+	accrual := float64(100)
+	uploadedAt := time.Now()
+
+	order := &app.Order{
+		ID:         1,
+		UserID:     userIDWithOrders,
+		Number:     orderNumber,
+		Status:     status,
+		Accrual:    &accrual,
+		UploadedAt: time.Now(),
+	}
+	orderStr := fmt.Sprintf(`[{"number": "%s", "status": "%s", "accrual": %f, "uploaded_at": "%s"}]`,
+		orderNumber,
+		status,
+		accrual,
+		uploadedAt.Format(time.RFC3339),
+	)
+
+	type request struct {
+		ctx context.Context
+	}
+
+	type want struct {
+		statusCode      int
+		contentType     string
+		responseBodyStr string
+	}
+
+	tests := []struct {
+		name    string
+		request request
+		want    want
+	}{
+		{
+			name: "user with orders",
+			request: request{
+				ctx: context.WithValue(context.Background(), UserIDKey, userIDWithOrders),
+			},
+			want: want{
+				statusCode:      http.StatusOK,
+				contentType:     ApplicationJSONKey,
+				responseBodyStr: orderStr,
+			},
+		},
+		{
+			name: "user without orders",
+			request: request{
+				ctx: context.WithValue(context.Background(), UserIDKey, userIDWithoutOrders),
+			},
+			want: want{
+				statusCode:      http.StatusNoContent,
+				contentType:     "",
+				responseBodyStr: "",
+			},
+		},
+		{
+			name: "unauthorized",
+			request: request{
+				ctx: context.Background(),
+			},
+			want: want{
+				statusCode:      http.StatusUnauthorized,
+				contentType:     "",
+				responseBodyStr: "",
+			},
+		},
+	}
+
+	// создаём контроллер
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// создаём объект-заглушку
+	m := mock.NewMockAppUsecaseInterface(ctrl)
+
+	// гарантируем, что заглушка
+	// при вызове с аргументом "Key" вернёт "Value"
+	m.EXPECT().GetOrders(userIDWithOrders).Return([]*app.Order{order}, nil)
+	m.EXPECT().GetOrders(userIDWithoutOrders).Return([]*app.Order{}, nil)
+
+	appHandler := NewAppHandler(m)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bodyReader := bytes.NewReader(nil)
+			req := httptest.NewRequest(http.MethodGet, TestHost+"/api/user/orders", bodyReader)
+			req = req.WithContext(tt.request.ctx)
+
+			w := httptest.NewRecorder()
+
+			appHandler.GetOrders(w, req)
+
+			res := w.Result()
+
+			assert.Equal(t, tt.want.statusCode, res.StatusCode, "Invalid status code")
+
+			if res.StatusCode == http.StatusOK {
+				assert.Contains(t, res.Header.Values(ContentTypeKey), tt.want.contentType)
+
+				defer res.Body.Close()
+				resBody, err := io.ReadAll(res.Body)
+				require.NoError(t, err)
+				assert.JSONEq(t, tt.want.responseBodyStr, string(resBody))
+			}
 		})
 	}
 }
